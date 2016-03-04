3c77105f708e8fcfb82036b28c66d87f9c5c20a2
huangjian <huan045@SHAW8NH1886.asycn.loc>
2016-03-04 16:27:01 +0800

    gggggggggggg

---
+++ testtt/src/Test.java
@@ -0,0 +1,22 @@
+/*******************************************************************
+ * Copyright (c) 2007 arvato and others
+ * All rights reserved.
+ *
+ * Contributors:
+ * arvato Systems (Shanghai) Co., Ltd.
+ * 
+ ******************************************************************/
+
+/**
+ * TODO (description of the Class)
+ *
+ * @author huan045
+ *
+ * @version 2016-3-4
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
